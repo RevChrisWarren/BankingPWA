719c2e32fd1c7f886c6df7a2df4ebd9807c01d3b
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-06-13 18:12:30 -0500

    files correctly added to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,9 @@
 const FILES_TO_CACHE = [
     "./index.html",
-    "../../public/css/style.css",
-    "./public/js/index.js",
-    "./public/js/idb.js",
-    "./routes/api.js",
-    "./models/transaction.js",
-    "../../server.js"
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js"
+
 ];
 const APP_PREFIX = "BankingPWA-";
 const VERSION = 'version_01';
@@ -16,6 +14,40 @@ self.addEventListener('install', function (e) {
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeepList.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
         })
     )
 })
