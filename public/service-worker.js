0b0a966fb64771f6b38072d82404501d3de6a25d
RevChrisWarren <chriswarren@Chriss-Mini-6.attlocal.net>
2022-06-15 21:33:48 -0500

    icons added to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,55 +3,61 @@ const FILES_TO_CACHE = [
     "./css/styles.css",
     "./js/index.js",
     "./js/idb.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
+
 
 ];
-const APP_PREFIX = "BankingPWA-";
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION
+const CACHE_NAME = 'my-site-cache-v2'
+const DATA_CACHE_NAME = 'data-cache-v2'
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) { //if cache is available, respond with cache
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else { //if there are no cache, try fetching request
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch(e.request)
-            }
-        })
-    )
-})
+
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were pre-cached successfully!');
+            return cache.addAll(FILES_TO_CACHE);
         })
-    )
-
-})
+    );
+    self.skipWaiting
+});
 
 self.addEventListener('activate', function (e) {
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            let cacheKeepList = keyList.filter(function (key) {
-                return key.indexOf(APP_PREFIX);
-            });
-            cacheKeepList.push(CACHE_NAME);
-
-
+        caches.keys().then(keyList => {
             return Promise.all(
-                keyList.map(function (key, i) {
-                    if (cacheKeepList.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
                     }
                 })
             );
+
+        })
+    )
+    self.clients.claim();
+});
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        fetch(e.request).catch(function () {
+            return caches.match(e.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (e.request.headers.get("accept").includes("text/html")) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match("/");
+                }
+            });
         })
     );
 });
